dec7947c75a42713b19d8105d95dd682c232eb6f
Brian Saputra <briansaputra078@gmail.com>
2026-01-10 21:17:14 +0700

    Tambah Migration Tabel Boking

---
+++ app/Models/Booking.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Booking extends Model
+{
+    use HasFactory;
+
+    protected $table = 'bookings';
+
+    protected $fillable = [
+        'nama_pemboking',
+        'dokter_id',
+        'tanggal_booking',
+        'kode_booking',
+        'status',
+        'keluhan',
+    ];
+
+    public function scopeStatus($query, $status)
+    {
+        return $query->where('status', $status);
+    }
+
+    protected static function booted()
+    {
+        static::creating(function ($booking) {
+            if (empty($booking->kode_booking)) {
+                $booking->kode_booking = self::generateKodeBooking();
+            }
+        });
+    }
+
+    public static function generateKodeBooking()
+    {
+        $tanggal = now()->format('Ymd');
+        $random  = strtoupper(Str::random(4));
+
+        return "BK-{$tanggal}-{$random}";
+    }
+
+    public function dokter()
+    {
+        return $this->belongsTo(Dokter::class);
+    }
+}
